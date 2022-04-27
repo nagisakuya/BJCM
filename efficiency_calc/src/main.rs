8e165927f756292711788b6dbfd4790de9ff1bae
nagisakuya <nagisakuya@gmail.com>
2022-04-27 16:38:30 +0900

    defined_strategy完成

--- efficiency_calc/src/main.rs
+++ efficiency_calc/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+fn func(range:impl Iterator<Item = usize>){
+    for _ in range{
+        todo!();
+    }
+}
+
+fn main(){
+    func(0..10);
+    func(0..=9);
 }
